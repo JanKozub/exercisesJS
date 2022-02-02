479d7aff3c8b2aa828b8efadd23b79cdbe0be619
Jan <j.kozub136@gmail.com>
2022-02-02 16:09:23 +0100

    -Minor changes

--- m/dratewka/script/Main.js
+++ m/dratewka/script/Main.js
@@ -2,17 +2,27 @@ import {Field} from "./Field.js";
 
 export class Game {
     map = Array.from(Array(6), _ => Array(7).fill(undefined));
+    position = {w: 6, h: 3}
 
     start() {
         console.log("game started!")
 
         this.genBoard();
-        console.log(this.map)
+
+        this.renderBoard();
     }
 
     genBoard() {
         for (const e of data.entries) {
             this.map[e.h][e.w] = new Field(e.w, e.h, e.msg, e.color, e.dir);
         }
+    }
+
+    renderBoard() {
+        console.log(this.map)
+        let currentField = this.map[this.position.h][this.position.w];
+        document.getElementById('msg-text').innerText = currentField.msg;
+        document.getElementById('dir-text').innerText = currentField.dir;
+        document.getElementById('main-image').style.backgroundColor = currentField.color;
     }
 }
