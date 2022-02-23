840beb7a428f43723060d122eb95c9e0260ed94c
JanKozub <j.kozub136@gmail.com>
2022-02-23 15:15:33 +0100

    added items

---
+++ m/dratewka/assets/items.js
@@ -0,0 +1,172 @@
+let items = {
+    "items": [
+        {
+            "id": 10,
+            "title": "a KEY",
+            "flag": 1,
+            "name": "KEY"
+        },
+        {
+            "id": 11,
+            "title": "an AXE",
+            "flag": 1,
+            "name": "AXE"
+        },
+        {
+            "id": 12,
+            "title": "STICKS",
+            "flag": 1,
+            "name": "STICKS"
+        },
+        {
+            "id": 13,
+            "title": "sheeplegs",
+            "flag": 0,
+            "name": "sheeplegs"
+        },
+        {
+            "id": 14,
+            "title": "MUSHROOMS",
+            "flag": 1,
+            "name": "MUSHROOMS"
+        },
+        {
+            "id": 15,
+            "title": "MONEY",
+            "flag": 1,
+            "name": "MONEY"
+        },
+        {
+            "id": 16,
+            "title": "a BARREL",
+            "flag": 1,
+            "name": "BARREL"
+        },
+        {
+            "id": 17,
+            "title": "a sheeptrunk",
+            "flag": 0,
+            "name": "sheeptrunk"
+        },
+        {
+            "id": 18,
+            "title": "BERRIES",
+            "flag": 1,
+            "name": "BERRIES"
+        },
+        {
+            "id": 19,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 20,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 21,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 22,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 23,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 24,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 25,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 26,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 27,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 28,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 29,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 30,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 31,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 32,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 33,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 34,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 35,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 36,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        },
+        {
+            "id": 37,
+            "title": "",
+            "flag": 0,
+            "name": ""
+        }
+    ]
+}
