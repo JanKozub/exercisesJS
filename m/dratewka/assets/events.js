8773bddf98396ca33c67573a3612766f01166f76
JanKozub <j.kozub136@gmail.com>
2022-03-04 10:21:42 +0100

    -added events

---
+++ m/dratewka/assets/events.js
@@ -0,0 +1,175 @@
+let items = {
+    "events": [
+        {
+            "w": 5,
+            "h": 4,
+            "used": 10,
+            "result": 11,
+            "message": "You opened a tool shed and took an axe",
+            "milestone": 0,
+            "new": 0
+        },
+        {
+            "w": 6,
+            "h": 5,
+            "used": 11,
+            "result": 12,
+            "message": "You cut sticks for sheeplegs",
+            "milestone": 0,
+            "new": 0
+        },
+        {
+            "w": 2,
+            "h": 3,
+            "used": 12,
+            "result": 13,
+            "message": "You prepared legs for your fake sheep",
+            "milestone": 1,
+            "new": 1
+        },
+        {
+            "w": 3,
+            "h": 2,
+            "used": 14,
+            "result": 15,
+            "message": "The tavern owner paid you money",
+            "milestone": 0,
+            "new": 0
+        },
+        {
+            "w": 6,
+            "h": 2,
+            "used": 15,
+            "result": 16,
+            "message": "The cooper sold you a new barrel",
+            "milestone": 0,
+            "new": 0
+        },
+        {
+            "w": 2,
+            "h": 3,
+            "used": 16,
+            "result": 17,
+            "message": "You made a nice sheeptrunk",
+            "milestone": 1,
+            "new": 1
+        },
+        {
+            "w": 5,
+            "h": 2,
+            "used": 18,
+            "result": 19,
+            "message": "The butcher gave you wool",
+            "milestone": 0,
+            "new": 0
+        },
+        {
+            "w": 2,
+            "h": 3,
+            "used": 19,
+            "result": 20,
+            "message": "You prepared skin for your fake sheep",
+            "milestone": 1,
+            "new": 1
+        },
+        {
+            "w": 6,
+            "h": 4,
+            "used": 21,
+            "result": 22,
+            "message": "You used your tools to make a rag",
+            "milestone": 0,
+            "new": 23
+        },
+        {
+            "w": 2,
+            "h": 3,
+            "used": 22,
+            "result": 23,
+            "message": "You made a fake sheephead",
+            "milestone": 1,
+            "new": 0
+        },
+        {
+            "w": 0,
+            "h": 0,
+            "used": 24,
+            "result": 25,
+            "message": "You are digging... (t) and digging... (t) That's enough sulphur for you",
+            "milestone": 0,
+            "new": 0
+        },
+        {
+            "w": 2,
+            "h": 3,
+            "used": 25,
+            "result": 26,
+            "message": "You prepared a solid poison",
+            "milestone": 1,
+            "new": 1
+        },
+        {
+            "w": 0,
+            "h": 1,
+            "used": 27,
+            "result": 28,
+            "message": "You got a bucket full of tar",
+            "milestone": 0,
+            "new": 0
+        },
+        {
+            "w": 2,
+            "h": 3,
+            "used": 28,
+            "result": 29,
+            "message": "You prepared a liquid poison",
+            "milestone": 1,
+            "new": 1
+        },
+        {
+            "w": 2,
+            "h": 3,
+            "used": "6xOK",
+            "result": 37,
+            "message": "Your fake sheep is full of poison and ready to be eaten by the dragon",
+            "milestone": 0,
+            "new": 0
+        },
+        {
+            "w": 2,
+            "h": 3,
+            "used": 37,
+            "result": 30,
+            "message": "The dragon noticed your gift... (t) The dragon ate your sheep and died!",
+            "milestone": 0,
+            "new": 1
+        },
+        {
+            "w": 2,
+            "h": 3,
+            "used": 33,
+            "result": 34,
+            "message": "You cut a piece of dragon's skin",
+            "milestone": 0,
+            "new": 0
+        },
+        {
+            "w": 6,
+            "h": 4,
+            "used": 34,
+            "result": 35,
+            "message": "You used your tools to make shoes",
+            "milestone": 0,
+            "new": 0
+        },
+        {
+            "w": 0,
+            "h": 3,
+            "used": 35,
+            "result": 36,
+            "message": "The King is impressed by your shoes",
+            "milestone": 0,
+            "new": 0
+        }
+    ]
+}
